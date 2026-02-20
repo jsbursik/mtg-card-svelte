c0b6f0fcf0074f692e353df1b204eec08479b873
Jordan Bursik <jsbursik@gmail.com>
2026-02-19 21:14:22 -0600

    Showing a sample card to verify StandardCard works

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
-  // Nothing yet
+  import StandardCard from "./lib/StandardCard.svelte";
 </script>
 
-<main>Nothing to see here so far...</main>
+<main>
+  <StandardCard
+    cardName="Llanowar Elves"
+    cardArt="/Llanowar-Elves-MtG-Art.jpg"
+    manaCost={["G"]}
+    typeLine="Creature — Elf Druid"
+    rulesText={`{T}: Add {G}.`}
+    flavorText="As patient and generous as life, as harsh and merciless as nature."
+    power="1"
+    toughness="1"
+    cardNumber="101"
+    totalCards="136"
+    rarity="C"
+    setCode="GN3"
+    language="EN"
+    artist="Chris Rahn"
+    year="2022"
+  />
+</main>
