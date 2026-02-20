bcc6a80ff712d8950de6c252fba20fe5d77c90de
Jordan Bursik <jsbursik@gmail.com>
2026-02-20 15:19:04 -0600

    new MtgCard component to use instead of each individual type

---
+++ src/lib/MtgCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { MtgCardProps } from "./types";
+  import StandardCard from "./StandardCard.svelte";
+  import PlaneswalkerCard from "./PlaneswalkerCard.svelte";
+
+  let { ...props }: MtgCardProps = $props();
+</script>
+
+{#if props.frame === "planeswalker"}
+  <PlaneswalkerCard {...props} />
+{:else if props.frame === "standard"}
+  <StandardCard {...props} />
+{/if}
